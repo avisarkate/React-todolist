9c10347e49fca73aed4355d1008939ebb0a9125c
Avinash Sarkate <sarkatea12@gmail.com>
2022-01-08 17:27:18 +0530

    first commit

---
+++ src/Component/ToDoInput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ToDoInput = () => {
+     return (
+          <div>
+               
+          </div>
+     )
+}
+
+export default ToDoInput
+
---
+++ src/Component/ToDuList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDuList = () => {
+     return (
+          <div>
+               
+          </div>
+     )
+}
+
+export default ToDuList
